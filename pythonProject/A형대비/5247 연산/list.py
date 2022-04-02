45728c96292a614ccab83a6b41f4fc2195c16e8b
puurib <roqkfroqkfgo@gmail.com>
2022-04-02 23:34:56 +0900

    0402

---
+++ pythonProject/A형대비/5247 연산/list.py
@@ -0,0 +1,4 @@
+a, b = [1, 3]
+
+print(a)
+print(b)
---
+++ pythonProject/A형대비/5247 연산/sol1.py
@@ -0,0 +1,34 @@
+'''
+5247. [파이썬 S/W 문제해결 구현] 6일차 - 연산 D4
+'''
+import sys
+sys.stdin = open('input.txt')
+from collections import deque
+
+
+def bfs(N, M):
+    # 큐 생성
+    queue = deque()
+    queue.append((N, 0))
+
+    check = {}
+    while queue:
+        item, count = queue.popleft()
+        if check.get(item, 0): continue
+        check[item] = 1
+        if item == M: return count
+        count += 1
+        if 0 < item + 1 <= 1000000:
+            queue.append((item + 1, count))
+        if 0 < item - 1 <= 1000000:
+            queue.append((item - 1, count))
+        if 0 < item * 2 <= 1000000:
+            queue.append((item * 2, count))
+        if 0 < item - 10 <= 1000000:
+            queue.append((item - 10, count))
+
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    rlt = bfs(N, M)
+    print(f'#{tc} {rlt}')
